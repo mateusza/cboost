2d213a8c5fd76d0c991b5e823ac47855e60e8392
Mateusz Adamowski <mateusz@adamowski.pl>
2021-12-31 22:37:41 +0100

    more test code try_cboost

--- try_cboost.py
+++ try_cboost.py
@@ -3,7 +3,14 @@
 import ast
 import cboost
 
-m1 = ast.parse("a = 123456")
+code = """
+a: int = 123
+b = 546
+"""
+
+m1 = ast.parse(code)
 m2 = cboost.convert(m1)
 
 print(m2)
+print(ast.dump(m1, indent=4))
+print(cboost.dump(m2, indent=4))
