b3bd69fb9dd9fc1c9de1b3a53c2e7435ccb00f1f
Mateusz Adamowski <mateusz@adamowski.pl>
2022-01-02 15:22:15 +0100

    try_cboost sandbox file

--- try_cboost.py
+++ try_cboost.py
@@ -2,15 +2,29 @@
 
 import ast
 import cboost
+import inspect
 
-code = """
-a: int = 123
-b = 546
-"""
+def is_prime(n: int) -> bool:
+    if n == 0 or n == 1:
+        return False
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+    for d in range(3, n):
+        if n % d == 0:
+            return False
+        if d * d > n:
+            break
+    return True
+
+code = inspect.getsource(is_prime)
 
 m1 = ast.parse(code)
-m2 = cboost.convert(m1)
-
-print(m2)
 print(ast.dump(m1, indent=4))
+
+m2 = cboost.convert(m1)
+print(m2)
 print(cboost.dump(m2, indent=4))
+
+print(cboost.render(m2, indent=4))
